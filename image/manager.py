ddac8b5a01ec06a0e5391e0acfbffa50c67c37a5
Scott Ernst <swernst@gmail.com>
2019-03-30 07:20:30 -0500

    Versioning
    Enhance version flexibility in manager.py.

--- image/manager.py
+++ image/manager.py
@@ -2,43 +2,54 @@ import argparse
 import os
 import subprocess
 
+import requests
+
 IMAGE = 'wiwdata/presto'
-TAG = '0.217'
+VERSION_URL = 'https://api.github.com/repos/prestodb/presto/tags'
 
 
-def build():
+def build(version: str):
     """Build image"""
     return subprocess.run([
         'docker', 'build',
-        '--tag', f'{IMAGE}:{TAG}',
-        '--build-arg', 'PRESTO_VERSION={}'.format(TAG),
+        '--tag', f'{IMAGE}:{version}',
+        '--build-arg', 'PRESTO_VERSION={}'.format(version),
         os.path.realpath(os.path.dirname(__file__))
     ])
 
 
-def run():
+def run(version: str):
     """Run container with bash shell."""
     return subprocess.run([
         'docker', 'run',
         '--rm', '-it',
-        f'{IMAGE}:{TAG}',
+        f'{IMAGE}:{version}',
         '/bin/bash'
     ])
 
 
-def push():
+def push(version: str):
     """Push image to docker hub."""
-    return subprocess.run(['docker', 'push', f'{IMAGE}:{TAG}'])
+    return subprocess.run(['docker', 'push', f'{IMAGE}:{version}'])
 
 
 def main():
     """Manages the presto image."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', choices=['build', 'run', 'push'])
+    parser.add_argument('action', choices=['build', 'run', 'push', 'version'])
+    parser.add_argument('--version')
     args = parser.parse_args()
 
+    version = args.version
+    if version is None:
+        version = requests.get(VERSION_URL).json()[0]['name']
+
+    if args.action == 'version':
+        print(f'Presto Version {version}')
+        return
+
     actions = {'build': build, 'run': run, 'push': push}
-    actions[args.action]()
+    actions[args.action](version)
 
 
 if __name__ == '__main__':
